fae27953a54150894360378403710449f77a0204
IIG BSCPLC NOC <noc@iig.bscplc>
2026-04-13 06:56:44 +0600

    fix: jest setupFilesAfterEnv typo

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'jest'
 const config: Config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
   transform: { '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }] },
 }
